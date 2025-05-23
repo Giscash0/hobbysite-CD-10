e810fede325ed6bbded3efb6203aedf1b2ea9a12
Giscash0 <jmgpiscasio@gmail.com>
2025-05-24 03:25:30 +0800

    created CommissionForm, JobForm, and JobApplicationForm

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from .models import Commission, Job, JobApplication
+
+class CommissionForm(forms.ModelForm):
+    pass
+
+class JobForm(forms.ModelForm):
+    pass
+
+class JobApplicationForm(forms.ModelForm):
+    pass
