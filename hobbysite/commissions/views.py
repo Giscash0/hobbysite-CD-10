a3de20c695aa192f542f8b9aaec7b14e2bf94226
Giscash0 <jmgpiscasio@gmail.com>
2025-05-24 03:35:22 +0800

    deleted commentlist and commentdetail views

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -3,17 +3,3 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from .models import Commission, Job, JobApplication
 from .forms import CommissionForm, JobForm, JobApplicationForm
-
-class CommentListView(ListView):
-    model = Commission
-    template_name = 'comment_list.html'
-    context_object_name = 'commissions'
-
-class CommentDetailView(DetailView):
-    model = Commission
-    template_name = 'comment_entry.html'
-    
-    def get_context_data(self, **args):
-        context = super().get_context_data(**args)
-        context['comments'] = Comment.objects.filter(commission=self.object)
-        return context
