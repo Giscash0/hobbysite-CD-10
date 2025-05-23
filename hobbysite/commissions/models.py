f47e98be1420d81af42232727c561f3b60abe4e0
Giscash0 <jmgpiscasio@gmail.com>
2025-05-24 02:42:47 +0800

    removed people_required field in Commission model

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -12,7 +12,6 @@ class Commission(models.Model):
     title = models.CharField(max_length = 255)
     description = models.TextField(blank = False)
     status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='open')
-    people_required = models.PositiveBigIntegerField()
     created_on = models.DateTimeField(auto_now_add = True)
     updated_on = models.DateTimeField(auto_now = True)
 
