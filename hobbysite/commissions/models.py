f666d5e2b2d2683d16ed5df63ec4dadcf38effcc
Giscash0 <jmgpiscasio@gmail.com>
2025-05-24 04:29:16 +0800

    fixed models.py readability

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -16,7 +16,7 @@ class Commission(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='authored_commissions')
-    
+
     def __str__(self):
         return self.title
 
@@ -32,10 +32,13 @@ class Job(models.Model):
         ('full', 'Full'),
     )
 
-    commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='commission')
+    commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='jobs')
     role = models.CharField(max_length=255)
     manpower_required = models.PositiveIntegerField()
     status = models.CharField(max_length=4, choices=STATUS_CHOICES, default='open')
+
+    def __str__(self):
+        return f"{self.role} ({self.manpower_required} needed)"
 
     class Meta:
         ordering = ['status', '-manpower_required', 'role']
@@ -46,8 +49,9 @@ class JobApplication(models.Model):
         ('accepted', 'Accepted'),
         ('rejected', 'Rejected')
     )
-    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='job')
-    applicant = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='applicant')
+
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applications')
+    applicant = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='job_applications')
     status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='pending')
     applied_on = models.DateTimeField(auto_now_add=True)
 
