32e508b6eda98d8be4cb148283954dcef047a299
Giscash0 <jmgpiscasio@gmail.com>
2025-05-24 05:43:34 +0800

    implemented urlpatterns

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from .views import commissions_list, commissions_detail, commissions_create, commissions_update, job_manage
 
 urlpatterns = [
-    path('list/', CommentListView.as_view(), name = 'comment_list'), 
-    path('detail/<int:pk>/', CommentDetailView.as_view(), name = 'comment_detail'),
+    path('list/', commissions_list, name='list'),
+    path('detail/<int:pk>/', commissions_detail, name='detail'),
+    path('add/', commissions_create, name='create'),
+    path('<int:pk>/edit/', commissions_update, name='update'),
+    path('jobs/<int:job_pk>/manage/', job_manage, name='manage'),
 ]
 
 app_name = "commissions"
